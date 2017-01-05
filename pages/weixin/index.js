9e3024edeb119f1b82c7f6b60f551e147b4500f1
Javen <914301050@qq.com>
2017-01-05 16:40:10 +0800

    聊天页面

--- pages/weixin/index.js
+++ pages/weixin/index.js
@@ -1,6 +1,94 @@
 // pages/weixin/index.js
 Page({
-  data:{},
+  data:{
+    chatList: [
+      {
+         type: "SYSTEM",
+         title: '公众号安全助手',
+         name: '',
+         msg: '公众号登录提醒',
+         time: '16:46',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      },{
+         type: "SYSTEM",
+         title: '微信团队',
+         name: '',
+         msg: '登录操作提醒',
+         time: '16:35',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '群组1',
+         name: 'A',
+         msg: 'xxxxx',
+         time: '16:41',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '公众号安全助手',
+         name: '',
+         msg: '公众号登录提醒',
+         time: '16:46',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      },{
+         type: "GROUP",
+         title: '微信团队',
+         name: '',
+         msg: '登录操作提醒',
+         time: '16:35',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '群组1',
+         name: 'A',
+         msg: 'xxxxx',
+         time: '16:41',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "PRIVATE",
+         title: '飞哥',
+         name: '飞哥',
+         msg: 'XXXXXX',
+         time: '16:46',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      },{
+         type: "GROUP",
+         title: '微信团队',
+         name: '',
+         msg: '登录操作提醒',
+         time: '16:35',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '群组1',
+         name: 'A',
+         msg: 'xxxxx',
+         time: '16:41',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '公众号安全助手',
+         name: '',
+         msg: '公众号登录提醒',
+         time: '16:46',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      },{
+         type: "GROUP",
+         title: '微信团队',
+         name: '',
+         msg: '登录操作提醒',
+         time: '16:35',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }, {
+         type: "GROUP",
+         title: '群组33',
+         name: 'B',
+         msg: 'xxxxx',
+         time: '16:41',
+         thumbnail: '../../asserts/images/thumbnail.png'
+      }
+    ]
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
